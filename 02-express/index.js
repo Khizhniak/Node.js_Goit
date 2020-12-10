1b0fed7d956ca3965efa447bcd31cf1989cb51d3
Khizhniak <ykhizhnyak@gmail.com>
2020-12-10 13:33:59 +0200

    hW

--- 02-express/index.js
+++ 02-express/index.js
@@ -16,81 +16,81 @@ app.use(express.json());
 
 const contactsPath = path.join(__dirname, "db\\contacts.json");
 
-// app.get("/", async (req, res) => {
-//   res.send("Hi")
-// });
+app.get("/", async (req, res) => {
+  res.send("Hi")
+});
 
-// app.get("/api/contacts", async (req, res) => {
-//   fs.readFile(contactsPath, "utf8", (err, data) => {
-//     if (err) throw err;
-//     const textFromJson = JSON.parse(data);
-//     return res.status(200).json({
-//       status: "success",
-//       code: 200,
-//       data: textFromJson,
-//     });
-//   });
-// });
+app.get("/api/contacts", async (req, res) => {
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) throw err;
+    const textFromJson = JSON.parse(data);
+    return res.status(200).json({
+      status: "success",
+      code: 200,
+      data: textFromJson,
+    });
+  });
+});
 
-// app.get(`/api/contacts/:id`, async (req, res) => {
-//   fs.readFile(contactsPath, "utf8", (err, data) => {
-//     if (err) throw err;
+app.get(`/api/contacts/:id`, async (req, res) => {
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) throw err;
 
-//     const textFromJson = JSON.parse(data);
-//     const idContactFromJSON = textFromJson.find(
-//       (item) => item.id == req.params.id
-//     );
-//     if (idContactFromJSON) {
-//       return res.status(200).json({
-//         status: "success",
-//         code: 200,
-//         data: idContactFromJSON,
-//       });
-//     } else {
-//       return res.status(404).json({
-//         code: 404,
-//         message: "Not found",
-//       });
-//     }
-//   });
-// });
+    const textFromJson = JSON.parse(data);
+    const idContactFromJSON = textFromJson.find(
+      (item) => item.id == req.params.id
+    );
+    if (idContactFromJSON) {
+      return res.status(200).json({
+        status: "success",
+        code: 200,
+        data: idContactFromJSON,
+      });
+    } else {
+      return res.status(404).json({
+        code: 404,
+        message: "Not found",
+      });
+    }
+  });
+});
 
-// app.post('/api/contacts', async (req, res) => {
-//   if( "name", "email", "phone" in req.body ){
-//     fs.readFile(contactsPath, "utf8", (err, data) => {
-//       if (err) throw err;
-//       const textFromJson = JSON.parse(data);
-//       req.body.id = 11;
-//       textFromJson.push(req.body)
-//       return res.status(200).send(textFromJson);
-//   })
-//   } else {
-//     return res.status(404).json({
-//       status: "nope",
-//       code: 404,
-//       data: req.body
-//     });
-//   }
-// });
+app.post('/api/contacts', async (req, res) => {
+  if( "name", "email", "phone" in req.body ){
+    fs.readFile(contactsPath, "utf8", (err, data) => {
+      if (err) throw err;
+      const textFromJson = JSON.parse(data);
+      req.body.id = 11;
+      textFromJson.push(req.body)
+      return res.status(200).send(textFromJson);
+  })
+  } else {
+    return res.status(404).json({
+      status: "nope",
+      code: 404,
+      data: req.body
+    });
+  }
+});
 
 
-// app.delete('/api/contacts/:id', async (req, res) => {
-//   fs.readFile(contactsPath, "utf8", (err, data) => {
-//     const textFromJson = JSON.parse(data);
-//     if (textFromJson.find(item => item.id == req.params.id)) {
-//       if (err) throw err;
-//       const filterContacts = textFromJson.filter(item => item.id !== req.params.id);
-//       return res.status(200).json({
-//         message: "contact deleted"
-//       })
-//     } else {
-//       return res.status(404).json({
-//         message: "Not found"
-//       })
-//     }
+app.delete('/api/contacts/:id', async (req, res) => {
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    const textFromJson = JSON.parse(data);
+    if (textFromJson.find(item => item.id == req.params.id)) {
+      if (err) throw err;
+      const filterContacts = textFromJson.filter(item => item.id !== req.params.id);
+      return res.status(200).json({
+        message: "contact deleted"
+      })
+    } else {
+      return res.status(404).json({
+        message: "Not found"
+      })
+    }
 
-//   })
-// })
+  })
+})
 
 
 
